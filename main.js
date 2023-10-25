e64aebe84bfb98a974eeb1dbd00152b3798e8c27
Patino5 <106916823+Patino5@users.noreply.github.com>
2023-10-26 07:02:48 +1000

    Added a random joke function and btn that will display a single Joke on the page

--- main.js
+++ main.js
@@ -2,14 +2,28 @@ const jokeList = $('#jokeList');
 const favoritesList = $('#favoritesList');
 const searchInput = $('#searchInput');
 const searchButton = $('#searchBtn');
+const randomJokeBtn = $('#randomJokeBtn');
+let pageCount = 1
 
-function searchForJokes(searchTerm) {
+function searchForJokes(searchTerm, pageCount) {
     // Set the Accept header to request JSON data
-    $.get(`https://icanhazdadjoke.com/search?term=${searchTerm}`, (data) => {
-        console.log(data.results); // Access the array of jokes in the response
+    $.get(`https://icanhazdadjoke.com/search?term=${searchTerm}&page=${pageCount}`, (data) => {
+        console.log(data.next_page); // Access the array of jokes in the response
         addJokeToJokeList(data.results); // Pass the results to the display function
     }, "json");
 }
+
+// Get a random joke and display in the list
+function getRandomJoke() {
+    $.get('https://icanhazdadjoke.com/', (data) => {
+        console.log(data.joke);
+        const joke = data.joke;
+        addJokeToJokeList([{ joke }]);
+    }, 'json');
+}
+
+randomJokeBtn.click(getRandomJoke);
+
 // iterate over the array of joke objects andadd each to list
 function addJokeToJokeList(arrayOfJokes) {
     //clear joke list before adding new jokes
@@ -33,7 +47,7 @@ searchButton.click(() => {
     const searchTerm = searchInput.val();
     if (searchTerm) {
         searchForJokes(searchTerm);
-    } else searchForJokes('')
+    } else searchForJokes(searchTerm, pageCount++)
 });
 
 // can press Enter key or click
